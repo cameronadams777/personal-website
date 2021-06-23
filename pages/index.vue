2a1b798bb85b242b0764c8de49ac4deb6fd602ca
Cameron Adams <cameron.adams@hey.com>
2021-06-23 07:06:22 -0500

    feat: update all nuxt dependencies

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <section>
+    <PersonalDescription />
+  </section>
+</template>
+
+<script>
+import PersonalDescription from '@/components/personal-description'
+export default {
+  components: {
+    PersonalDescription,
+  },
+}
+</script>
